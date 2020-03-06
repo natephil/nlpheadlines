d7052a6ea9c78a913bf9ec083db5037c398c77ad
Nathan Cahn <nathancahn@Nathans-MBP-2.ucsf.edu>
2020-03-06 11:01:49 -0800

    new scraper model

--- scraping/management/commands/scrape.py
+++ scraping/management/commands/scrape.py
@@ -5,28 +5,50 @@ from django.core.management.base import BaseCommand
 
 from scraping.models import Headline
 
+# define news website
+nytimes = Newscatcher('nytimes.com')
+usa_today = Newscatcher('usatoday.com')
+la_times = Newscatcher('latimes.com')
+guardian = Newscatcher('theguardian.com')
+wash_post = Newscatcher('washingtonpost.com')
+daily_mail = Newscatcher('dailymail.co.uk')
+NBC = Newscatcher('nbcnews.com')
+fox = Newscatcher('foxnews.com')
+huff_post = Newscatcher('huffpost.com')
+google_news = Newscatcher('news.google.com')
+wired = Newscatcher('wired.com')
+
+news_list = [usa_today, la_times, guardian, wash_post, daily_mail, NBC, fox, huff_post, google_news, nytimes, wired]
+
 class Command(BaseCommand):
     help = "collect headlines"
-        
+
     # define logic of command
     def handle(self, *args, **options):
+
+        for n in news_list:
+            news = n.news
+            source = n.website
+            for i in news:
+                title = i.title
+                published_date = i.published
         
-        # define news website
-        nytimes = Newscatcher('nytimes.com')
-    
+
         # get headlines
-        news = nytimes.news
-        
+        # news = nytimes.news
+
         # grab all headlines
-        for n in news:
-            title = n.title
-            published_date = n.published
+        # for n in news:
+            # source = news.website
+            # title = n.title
+            # published_date = n.published
             # published_date = datetime.strptime(n.published, "%a, %d %b %Y %I:%M:%S %z").strftime("%Y-%m-%d")
 
             # check if url in db
             try:
                 # save in db
                 Headline.objects.create(
+                    source=source,
                     title=title,
                     published_date=published_date
                 )
@@ -34,4 +56,4 @@ class Command(BaseCommand):
             except:
                 print('%s already exists' % (title,))
 
-        self.stdout.write( '\n\Finished Collecting Headlines!' )
+        self.stdout.write( '\n\nFinished Collecting Headlines!' )
--- scraping/models.py
+++ scraping/models.py
@@ -4,7 +4,8 @@ from django.utils import timezone
 # Create your models here.
 
 class Headline(models.Model):
-    title = models.CharField(max_length=250)
+    source = models.Charfield(max_length=250)
+    title = models.CharField(max_length=350)
     published_date = models.CharField(max_length=250)
     # published_date = models.DateTimeField()
     def __str__(self):
