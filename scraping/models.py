b9b090b0c7792c6ca698abd19182f0f2ef09df14
Nathan Cahn <nathancahn@Nathans-MBP-2.ucsf.edu>
2020-03-06 11:04:41 -0800

    updated model Charfield

--- scraping/models.py
+++ scraping/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 class Headline(models.Model):
     source = models.Charfield(max_length=250)
-    title = models.CharField(max_length=350)
+    title = models.CharField(max_length=250)
     published_date = models.CharField(max_length=250)
     # published_date = models.DateTimeField()
     def __str__(self):
