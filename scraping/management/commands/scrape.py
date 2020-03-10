f8caae67537a4192a0c8e258067b3dba62cdf591
Nathan Cahn <nathancahn@Nathans-MBP-2.ucsf.edu>
2020-03-09 22:46:57 -0700

    fixed scraper

--- scraping/management/commands/scrape.py
+++ scraping/management/commands/scrape.py
@@ -27,24 +27,45 @@ class Command(BaseCommand):
     # define logic of command
     def handle(self, *args, **options):
 
+        for n in news_list:
+            news = n.news
+            source = n.website
+            for i in news:
+                title = i.title
+                # published_date = i.published
+                published_date = datetime.fromtimestamp(mktime(i.published_parsed))
+
         # get headlines
-        news = nytimes.news
+        # news = nytimes.news
 
         # grab all headlines
-        for n in news:
-            source = news.website
-            title = n.title
-            published_date = datetime.fromtimestamp(mktime(i.published_parsed))
-            # store data in db
+        # for n in news:
+            # source = news.website
+            # title = n.title
+            # published_date = n.published
+            # published_date = datetime.fromtimestamp(mktime(i.published_parsed))
+
+                # check if url in db
+                exists = False
                 try:
-                    # save in db
-                    Headline.objects.create(
+                    exists = Headline.objects.get(
                         source=source,
                         title=title,
                         published_date=published_date
                     )
-                    print('"%s" added to database' % (title))
                 except:
-                    print('"%s" Error storing duplicate value in database' % (title,))
+                    pass
+                # store data in db
+                if exists == False:
+                    try:
+                        # save in db
+                        Headline.objects.create(
+                            source=source,
+                            title=title,
+                            published_date=published_date
+                        )
+                        print('"%s" added to database' % (title))
+                    except:
+                        print('"%s" Error storing duplicate value in database' % (title,))
 
         self.stdout.write( '\n\nFinished Collecting Headlines!' )
--- scraping/models.py
+++ scraping/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 class Headline(models.Model):
     source = models.CharField(max_length=250, default="default_value")
-    title = models.CharField(max_length=350)
+    title = models.CharField(max_length=500)
     # published_date = models.CharField(max_length=250)
     published_date = models.DateTimeField()
     def __str__(self):
